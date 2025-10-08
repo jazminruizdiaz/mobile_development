ccf9ec1d00af07ea5d5ab33a6f66e8aa64161d86
fabri10roman <fabri11roman.fr@gmail.com>
2025-10-08 10:09:41 -0300

    add home sreen

---
+++ App.tsx
@@ -0,0 +1,12 @@
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { Home } from './src/screens/Home';
+
+function App() {
+  return (
+    <SafeAreaProvider>
+      <Home />
+    </SafeAreaProvider>
+  );
+}
+
+export default App;
