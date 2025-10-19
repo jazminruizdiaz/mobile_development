81fd675e11f24557fead4663fbc9f845912187dd
AndresSuresoft <andres.gaona@assuresoft.tech>
2025-10-19 16:34:37 -0300

    Added wishlist list movies

--- src/screens/Wishlist/Wishlist.tsx
+++ src/screens/Wishlist/Wishlist.tsx
@@ -2,14 +2,25 @@ import React from 'react';
 import { View, StyleSheet } from 'react-native';
 import { TextCustom } from '../../components/atoms/Text/TextCustom';
 import { colors } from '../../constants/colors';
+import { useWishlist } from '../../contexts/Wishlist/WishlistContext';
+import { MovieGrid } from '../Movie/components/grid/MovieGrid';
 
 const Wishlist = () => {
+  const { wishlist } = useWishlist();
+
   return (
     <View style={styles.container}>
       <TextCustom variant="title">Wishlist</TextCustom>
-      <TextCustom variant="body" style={styles.subtitle}>
-        Wishlist screen coming soon...
-      </TextCustom>
+
+      {wishlist.length > 0 ? (
+        <MovieGrid movies={wishlist} />
+
+      ) : (
+        <TextCustom variant="body" style={styles.subtitle}>
+          No movies in your Wishlist yet
+        </TextCustom>
+      )}
+
     </View>
   );
 };
@@ -21,7 +32,12 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     backgroundColor: colors.background,
   },
+  title: {
+    textAlign: 'center',
+    marginVertical: 16,
+  },
   subtitle: {
+    textAlign: 'center',
     marginTop: 8,
   },
 });
