4f42f8c86973311b00c27a6629fe241d0ecbea41
veronica <luzchaconq@gmail.com>
2025-10-19 22:10:50 -0400

    Creating context and hook to change theme

---
+++ src/contexts/Theme/ThemeProvider.tsx
@@ -0,0 +1,21 @@
+import React, { useState, ReactNode } from 'react';
+import { ThemeContext, ThemeContextType, ThemeMode } from './ThemeContext';
+
+export const ThemeProvider = ({ children }: { children: ReactNode }) => {
+  const [themeMode, setThemeMode] = useState<ThemeMode>('dark');
+
+  const toggleThemeMode = () => {
+    setThemeMode((prev) => (prev === 'dark' ? 'light' : 'dark'));
+  };
+
+  const value: ThemeContextType = {
+    themeMode,
+    toggleThemeMode,
+  };
+
+  return (
+    <ThemeContext.Provider value={value}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};
