2ad56b3df04f1b486e3bb9d2e6447146f4d658e9
Veronica Chacon <veronica.chacon@assuresoft.tech>
2025-10-23 12:37:26 -0400

    Fixing merge issues in movies.tsx

--- src/types/Movie.ts
+++ src/types/Movie.ts
@@ -1,11 +1,8 @@
-<<<<<<< HEAD
 export type Genre = {
   id: number;
   name: string;
 };
 
-=======
->>>>>>> 8e29b90ef1986ca26df28b7c5727ade57b8921d4
 export const DEFAULT_GENRE = 'all';
 
 export interface Movie {
@@ -14,9 +11,5 @@ export interface Movie {
   poster_path: string;
   vote_average: number;
   release_date: string;
-<<<<<<< HEAD
   genre_ids: number[];
-=======
-  genre_ids?: number[];
->>>>>>> 8e29b90ef1986ca26df28b7c5727ade57b8921d4
 }
